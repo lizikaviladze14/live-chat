2ce78df96b9f5d568b9ad3e84ef42a49b81fe284
lizikaviladze14 <lizikavila@gmail.com>
2024-09-20 14:55:09 +0400

    added .env file for firebase api keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBM3VBx-1t3Oza9L5NncZKryK729O6-m_k",
-    authDomain: "vue-firebase-d4433.firebaseapp.com",
-    projectId: "vue-firebase-d4433",
-    storageBucket: "vue-firebase-d4433.appspot.com",
-    messagingSenderId: "344166393412",
-    appId: "1:344166393412:web:5edbcd1ccf577d4b027e11"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 }
  
 // init firebase
